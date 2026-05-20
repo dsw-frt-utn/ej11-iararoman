0de38e016f97507c075438b07513b7920e49c092
iararoman <iara.roman@alu.frt.utn.edu.ar>
2026-05-20 19:12:16 -0300

    Se resolvio CasoList

--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,17 +1,73 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
 {
-    //Agregar 3 alumnos a la lista
-    //Listar por consola los alumnos
-    //Buscar por nombre un alumno que exista y mostrar por consola
-    //Buscar por nombre un alumno que no exista y mostrar por consola el texto "No existe"
-    //Eliminar un alumno y listar por consola los alumnos
-    //Eliminar el primer elemento de la lista y listar por consola los alumnos
+    
     public static void EjemploList()
     {
+        CasoList miCasoList = new CasoList();
 
+        //Agregar 3 alumnos a la lista
+        Alumno alumno1 = new Alumno(1, "Gabriel", 9.5);
+        Alumno alumno2 = new Alumno(2, "Iara", 8.5);
+        Alumno alumno3 = new Alumno(3, "Maria", 7);
+
+
+        miCasoList.AgregarAlumno(alumno1);
+        miCasoList.AgregarAlumno(alumno2);
+        miCasoList.AgregarAlumno(alumno3);
+
+        //Listar por consola los alumnos
+        Console.WriteLine("\nLista de Alumnos");
+
+        List<Alumno> listaCompleta = miCasoList.ObtenerLista();
+        foreach (Alumno alumno in listaCompleta)
+        {
+            Console.WriteLine(alumno);
+        }
+
+        //Buscar por nombre un alumno que exista y mostrar por consola
+        Console.WriteLine("\nBuscando Alumno...");
+
+        Alumno? alumnoEncontrado = miCasoList.BuscarAlumno("Gabriel");
+        if (alumnoEncontrado != null)
+        {
+            Console.WriteLine($"\nAlumno Encontado : {alumnoEncontrado}");
+        }
+
+        //Buscar por nombre un alumno que no exista y mostrar por consola el texto "No existe"
+        Console.WriteLine("\nBuscando Alumno...");
+
+        Alumno? alumnoNoEncontrado = miCasoList.BuscarAlumno("Pedro");
+        if (alumnoNoEncontrado == null)
+        {
+            Console.WriteLine("\nNo Existe");
+        }
+        //Eliminar un alumno y listar por consola los alumnos
+        Console.WriteLine("\nEliminando alumno...");
+
+        miCasoList.EliminarAlumno(alumno3);
+
+        List<Alumno> listaActualizada = miCasoList.ObtenerLista();
+        foreach (Alumno alumno in listaCompleta)
+        {
+            Console.WriteLine(alumno);
+        }
+
+        //Eliminar el primer elemento de la lista y listar por consola los alumnos
+        Console.WriteLine("\nEliminando alumno de la primera posición...");
+
+        miCasoList.EliminarAlumnoEnPosicion(0);
+
+        var listaFinal = miCasoList.ObtenerLista();
+        foreach (var alumno in listaFinal)
+        {
+            Console.WriteLine(alumno);
+        }
     }
 
     //Agregar 3 alumnos al diccionario
